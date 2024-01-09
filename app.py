b0c38ea1d46771e06cc68eb4e1363795480fe922
shahbazr12 <mdshahbazx2@gmail.com>
2024-01-09 18:17:06 +0530

    hi Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,jsonify,request
+import pickle
+import json
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/prediction',methods=['GET','post'])
+def prediction():
+    if request.method=="POST":
+        nitro = request.form.get("nitrogen")
+        phos = request.form.get("phosphorous")
+        kp = request.form.get("potassium")
+        temp = request.form.get("temperature")
+        hum = request.form.get("humidity")
+        ph =request.form.get("ph")
+        rain = request.form.get("rainfall")
+
+        print(nitro,phos,kp,temp,hum,ph,rain)
+        with open("model.pkl","rb") as model_file:
+            mlmodel=pickle.load(model_file)
+
+        res = mlmodel.predict([[float(nitro),float(phos),float(kp),float(temp),float(hum),float(ph),float(rain)]])
+        return render_template("result.html",res=res)
+    
+    
+
+
+    else:
+        return  render_template("prediction.html")
+
+
+if __name__=='__main__':
+    app.run()
+
+
